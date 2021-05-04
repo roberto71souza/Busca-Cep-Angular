a547c00e95375e8c2b06c5f9d5e2a838885db60d
Roberto de souza <roberto71souza@gmail.com>
2021-05-04 18:34:35 -0300

    utilizacao de 1 modal tanto para busca para ja buscados

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit, TemplateRef, Input, ViewEncapsulation } from '@angular/core';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 import { Cep } from '../models/cep';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
   @Input() cep!: Cep;
 
-  modalRef!: BsModalRef;
-  constructor(private modalService: BsModalService) {}
+  @ViewChild(ModalDirective, { static: false }) modal!: ModalDirective;
+  constructor() {}
   ngOnInit() {}
 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template,{class:'modalcustom modal-sm'});
+  showModal() {
+    this.modal.show();
   }
 }
